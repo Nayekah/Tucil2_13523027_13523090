7aca32ac2d939b0bbd1b3db03d30aa1aa058ffa0
Fajar2k5 <fajar2k5@gmail.com>
2025-04-05 12:19:21 +0700

    feat: update pixel hpp

--- src/header/Pixel.hpp
+++ src/header/Pixel.hpp
@@ -1,14 +1,20 @@
-#ifndef _PIXEL_H
-#define _PIXEL_H
+#ifndef _PIXEL_HPP
+#define _PIXEL_HPP
 
 
+// Handler for Pixel in image processing
 struct Pixel {
     unsigned char r;
     unsigned char g;
     unsigned char b;
     
-    Pixel() : r(0), g(0), b(0) {}
-    Pixel(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
+    // Default ctor (black)
+    Pixel() : r(0), g(0), b(0) {
+    }
+
+    // rgb ctor
+    Pixel(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {
+    }
     
     // Overload operators for pixel calculations
     Pixel operator+(const Pixel& other) const {

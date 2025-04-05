5a1711358f984fb6457fbd91dd5ca093e0f4f277
Fajar2k5 <fajar2k5@gmail.com>
2025-04-05 12:23:44 +0700

    feat: update compression analyzer

--- src/header/CompressionAnalyzer.hpp
+++ src/header/CompressionAnalyzer.hpp
@@ -1,26 +1,35 @@
-#ifndef _COMPRESSION_ANALYZER_H
-#define _COMPRESSION_ANALYZER_H
+#ifndef _COMPRESSION_ANALYZER_HPP
+#define _COMPRESSION_ANALYZER_HPP
 
+
+// include lib files
 #include <cstddef>
+#include <iostream>
+#include <iomanip>
+
+// include header file
 #include "QuadTree.hpp"
 
+
+// namespace
 using namespace std;
 
 class CompressionAnalyzer {
-public:
-    CompressionAnalyzer(size_t originalSize, size_t compressedSize, const QuadTree& quadTree);
-    ~CompressionAnalyzer();
-    
-    // Calculate compression metrics
-    double calculateCompressionPercentage() const;
-    
-    // Display results
-    void displayResults(double executionTimeSeconds) const;
-    
-private:
-    size_t originalImageSize;
-    size_t compressedImageSize;
-    const QuadTree& quadTree;
-};
+    public:
+        CompressionAnalyzer(size_t originalSize, size_t compressedSize, const QuadTree& quadTree); //Ctor
+        ~CompressionAnalyzer(); //Dtor
+        
+        // Calculate compression metrics
+        double calculateCompressionPercentage() const;
+        
+        // Display results
+        void displayResults(double executionTimeSeconds) const;
+        
+    private:
+        // Params
+        size_t originalImageSize;
+        size_t compressedImageSize;
+        const QuadTree& quadTree;
+    };
 
 #endif
